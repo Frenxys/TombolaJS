85eeda93cd278480cfe2dbbf60cd4e8f2f8353d0
Frenxys <eneazani2005@gmail.com>
2023-03-12 15:33:31 +0100

    finito

--- js/tombola.js
+++ js/tombola.js
@@ -1,27 +1,25 @@
 let numeri=[];//numeri generati
 let numero=0;
+tabella=document.getElementById("tabella")
 function stampaTabellone(){
-    document.write('<div class="grid-container">');
     for(let i=0; i<9; i++){
         for(let j=1; j<=10; j++){
             numero=j+(i*10);
             if(numeri.includes(numero)){
-                document.write("<div id='occupato' >"+numero+"</div>");
+                tabella.innerHTML+="<div id='occupato' >"+numero+"</div>";
             }else{
-                document.write("<div id='libero' >"+numero+"</div>");
+                tabella.innerHTML+="<div id='libero' >"+numero+"</div>";
             }
-            
         }
-        //document.write("<br>");
     }
-    document.write('</div>');
 }
 function pescaNumero(){
-    do{
-        n = Math.floor(Math.random() * 90) + 1;
-    }while(numeri.includes(n))
-    numeri.push(n);
-    console.log(n);
-    stampaTabellone();
+    let n = Math.floor(Math.random() * 90) + 1;
+    if(!numeri.includes(n)){
+        numeri.push(n);
+        console.log(n);
+        tabella.innerHTML="";
+        stampaTabellone();
+    }
 }
 stampaTabellone();
